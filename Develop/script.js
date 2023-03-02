645d454b3434ee34a905ee32289d63399d4fa49f
averybarela <120346753+averybarela@users.noreply.github.com>
2023-03-01 20:31:07 -0800

    Adding generate password function and notes

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,51 @@
 // Assignment code here
+ //these are the values that are going to be used to create a password
+const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
+const lowerCase ="abcdefghijklmnopqrstuvwxyz".split("")
+const specialCharacters= "!@#$%^&*()_+<>?".split("")
+const numeric="0123456789".split("")
+
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate"); 
 
-var upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("")
-console.log (upperCase)
-var lowerCase ="abcdefghijklmnopqrstuvwxyz".split("") 
-console.log(lowerCase)
-var specialCharacters="!@#$%^&*()_+<>{}[]".split("")
-console.log(specialCharacters) 
-var numeric ="0123456789".split("")
-console.log(numeric)
-var choices ="128" 
 
 // Write password to the #password input
-function writePassword() {
+// function writePassword() {
+//  var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+// passwordText.value = password;
+ 
+// }
+
+function generatePassword(){
+  var charSelection = " " // string methods or += operators
   var choices = prompt("How Many Password Characters?(min 8, max 128)");
+    while (choices < 8 || choices > 128) {
+      choices = prompt("How Many Password Characters?(min 8, max 128)");
+    }
   var upperCase =confirm('Would you like to include UpperCase Letters?')
+    if (uppercase) {
+
+    }
   var lowerCase =confirm('Would you like to include LowerCase Letters?')
+    if (lowerCase) {
+      
+  }
   var specialCharacters =confirm('Would you like to include Special Characters?')
+    if (specialCharacters) {
+      
+  }
   var numeric =confirm('Would you like to include numbers?')
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-  var randomIndex= Math.floor(Math.random()*choices.length);
+    if (numeric) {
+      
+  }
+  console.log (upperCase, lowerCase, specialCharacters, numeric)
+  // var randomIndex= Math.floor(Math.random()*choices.length);
 }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", generatePassword);
 
--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Password-Generator-w-JavaScript
 
-User Story
+In this project, my goal was to create a responsive password generator which prompted the user to input desired parameters for a password. 
+
+## User Story
+
 AS AN employee with access to sensitive data
 I WANT to randomly generate a password that meets certain criteria
 SO THAT I can create a strong password that provides greater security
 
-Accceptance Criteria
+## Accceptance Criteria
 GIVEN I need a new, secure password
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
@@ -21,3 +24,5 @@ WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
+
+![Screenshot of Application](./Develop/Screenshot_20230214_105641.png)
